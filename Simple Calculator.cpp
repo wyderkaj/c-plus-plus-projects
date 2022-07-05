0f35f83f748a344890328f1368b3a98f7974cd80
wyderkaj <59506352+wyderkaj@users.noreply.github.com>
2022-07-05 13:28:59 +0200

    Add files via upload
    Simple calculator that performs four operations on two given numbers

---
+++ Simple Calculator.cpp
@@ -0,0 +1,43 @@
+﻿// Simple Calculator.cpp : Ten plik zawiera funkcję „main”. W nim rozpoczyna się i kończy wykonywanie programu.
+//
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    char op;
+    double num1, num2;
+
+    cout << "Simple calculator that performs four operations on two given numbers.\n";
+    cout << "Enter the Operator (+, -, *, /): ";
+    cin >> op;
+
+    cout << "Enter two numbers one by one: ";
+    cin >> num1 >> num2;
+
+    switch (op) {
+
+    case '+':
+        cout << num1 << " + " << num2 << " = " << (num1 + num2);
+        break;
+    case '-':
+        cout << num1 << " - " << num2 << " = " << (num1 - num2);
+        break;
+    case '*':
+        cout << num1 << " * " << num2 << " = " << (num1 * num2);
+        break;
+    case '/':
+        if (num2 != 0.0)
+            cout << num1 << " / " << num2 << " = " << (num1 / num2);
+        else
+            cout << "Divide by Zero situation";
+
+        break;
+    default:
+        cout << op << "is an invalid operator";
+    }
+
+    return 0;
+}
